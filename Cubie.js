a304d5f8024b150097ac65d693ac23ae4db7e99b
Mistorcha <chassot.bruno@gmail.com>
2023-01-10 11:56:13 +0100

    working on
    stuff

--- Cubie.js
+++ Cubie.js
@@ -1,8 +1,58 @@
 class Cubie{
-    constructor(x, y, z, sideLength) {
-        this.sideLength = sideLength
+    constructor(i, j, k, x, y, z, sideLength) {
+        this.r = sideLength / 2
+        this.rr = this.r *0.9
+        this.posInSpace = [i, j, k]
         this.initialPos = createVector(x, y, z)
         this.pos = createVector(x, y, z)
+    
+        this.quads = []
+        this.fillQuads()
+    }
+
+    fillQuads() {
+        if(this.posInSpace[0] == 0) {
+            this.quads.push(["orange",
+                            [-this.r, -this.rr, -this.rr],
+                            [-this.r,  this.rr, -this.rr],
+                            [-this.r,  this.rr,  this.rr],
+                            [-this.r, -this.rr,  this.rr]])
+        }
+        if(this.posInSpace[0] == dim-1) {
+            this.quads.push(["red",
+                            [ this.r, -this.rr, -this.rr],
+                            [ this.r,  this.rr, -this.rr],
+                            [ this.r,  this.rr,  this.rr],
+                            [ this.r, -this.rr,  this.rr]])
+        }
+        if(this.posInSpace[2] == 0) {
+            this.quads.push(["blue",
+                            [-this.rr, -this.rr, -this.r],
+                            [ this.rr, -this.rr, -this.r],
+                            [ this.rr,  this.rr, -this.r],
+                            [-this.rr,  this.rr, -this.r]])
+        }
+        if(this.posInSpace[2] == dim-1) {
+            this.quads.push(["green",
+                            [-this.rr, -this.rr,  this.r],
+                            [ this.rr, -this.rr,  this.r],
+                            [ this.rr,  this.rr,  this.r],
+                            [-this.rr,  this.rr,  this.r]])
+        }
+        if(this.posInSpace[1] == 0) {
+            this.quads.push(["white",
+                            [-this.rr, -this.r, -this.rr],
+                            [ this.rr, -this.r, -this.rr],
+                            [ this.rr, -this.r,  this.rr],
+                            [-this.rr, -this.r,  this.rr]])
+        }
+        if(this.posInSpace[1] == dim-1) {
+            this.quads.push(["yellow",
+                            [-this.rr,  this.r, -this.rr],
+                            [ this.rr,  this.r, -this.rr],
+                            [ this.rr,  this.r,  this.rr],
+                            [-this.rr,  this.r,  this.rr]])
+        }
     }
 
     show() {
@@ -11,9 +61,16 @@ class Cubie{
         strokeWeight(2)
         push()
         translate(this.pos.x, this.pos.y, this.pos.z)
-        box(this.sideLength)
-
-
+        beginShape(QUADS);
+        for (let i = 0; i< this.quads.length; i++) {
+        fill(this.quads[i][0])
+        vertex(this.quads[i][1][0], this.quads[i][1][1], this.quads[i][1][2])
+        vertex(this.quads[i][2][0], this.quads[i][2][1], this.quads[i][2][2])
+        vertex(this.quads[i][3][0], this.quads[i][3][1], this.quads[i][3][2])
+        vertex(this.quads[i][4][0], this.quads[i][4][1], this.quads[i][4][2])
+        }
+        endShape();
+        
         pop()
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,45 +1,38 @@
 
 canvasWidth = 200
 canvasHeight = 200
-dim = 10;
+dim = 3;
 cube = []
 cubieSideLength = (canvasWidth/0.7) / dim;
 
-//TOP, BOTTOM, RIGHT, LEFT, FRONT, BACK
-color = ["white", "yellow", 'red', "orange", 'green', 'blue']
-
-
 
 function setup() {
+    let h = new p5.Matrix()
+    print(h)
     smooth()
-    createCanvas(200, 200, WEBGL);
+    createCanvas(canvasWidth, canvasHeight, WEBGL);
     createEasyCam();
     document.oncontextmenu = ()=>false;
 
 
     for         (i = 0; i < dim; i++) {
-        cube.push([])
         for     (j = 0; j < dim; j++) {
-            cube[i].push([])
             for (k = 0; k < dim; k++) {
-                offset = (dim - 1) * cubieSideLength / 2
-                x = cubieSideLength * i - offset;
-                y = cubieSideLength * j - offset;
-                z = cubieSideLength * k - offset
-                cube[i][j].push(new Cubie(x, y, z, cubieSideLength))
+                if(i == 0 || j == 0 || k == 0 || i == dim-1 || j == dim-1 || k == dim-1) {
+                    offset = (dim - 1) * cubieSideLength / 2
+                    x = cubieSideLength * i - offset;
+                    y = cubieSideLength * j - offset;
+                    z = cubieSideLength * k - offset
+                    cube.push(new Cubie(i, j, k, x, y, z, cubieSideLength))
+                }
             }
         }
     }
-
-  }
+}
   
   function draw() {
-    background(58)
-    for         (i = 0; i < dim; i++) {
-        for     (j = 0; j < dim; j++) {
-            for (k = 0; k < dim; k++) {
-                cube[i][j][k].show()
-            }
-        }
+    background(200)
+    for (i = 0; i < cube.length; i++) {
+        cube[i].show()
     }
-  }
+}
